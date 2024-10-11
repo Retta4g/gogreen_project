006615f08c531ac2404b17af456b9325f497159e
Retta4g <messiwhanna@gmail.com>
2024-10-11 13:16:58 -0500

    second

---
+++ key.tf
@@ -0,0 +1,14 @@
+# # Key Pair for the instance
+# resource "aws_key_pair" "deployer" {
+#   key_name   = "deployer-key"
+#   public_key = file("~/.ssh/id_ed25519.pub")
+# }
+# AWS Key Pair for Deployer
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer"
+  public_key = var.public_key_content  # You need to have a variable for the public key content
+}
+variable "public_key_content" {
+  description = "The public key to be used for the AWS key pair."
+  type        = string
+}
